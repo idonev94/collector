c9d9ceb756cf6744cad6169e65682e896ff51116
idonev94 <100535680+idonev94@users.noreply.github.com>
2025-11-06 15:43:03 +0200

    Create __init__.py

---
+++ assurance/assurance/einstein/__init__.py
@@ -0,0 +1,10 @@
+from .session import EinsteinSession
+from .types import (
+    Alert,
+    AlertEvent,
+    AlertKey,
+    AlertSeverity,
+    Einstein,
+    EinsteinMessage,
+    KeepAliveAlert,
+)
